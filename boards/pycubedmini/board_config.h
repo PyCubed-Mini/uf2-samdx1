4133ee99adb76f17afd9b34e5b4410aeacb5e768
Neil Khera <32021246+NeilKhera@users.noreply.github.com>
2023-12-26 18:07:41 -0500

    Fix UART Pins in board_config.h

--- boards/pycubedmini/board_config.h
+++ boards/pycubedmini/board_config.h
@@ -19,8 +19,8 @@
 #define BOOT_USART_PAD_SETTINGS           UART_RX_PAD1_TX_PAD0
 #define BOOT_USART_PAD3                   PINMUX_UNUSED
 #define BOOT_USART_PAD2                   PINMUX_UNUSED
-#define BOOT_USART_PAD1                   PINMUX_PA05D_SERCOM0_PAD1
-#define BOOT_USART_PAD0                   PINMUX_PA04D_SERCOM0_PAD0
+#define BOOT_USART_PAD1                   PINMUX_PA04D_SERCOM0_PAD0
+#define BOOT_USART_PAD0                   PINMUX_PA05D_SERCOM0_PAD1
 #define BOOT_GCLK_ID_CORE                 SERCOM0_GCLK_ID_CORE
 #define BOOT_GCLK_ID_SLOW                 SERCOM0_GCLK_ID_SLOW
 
